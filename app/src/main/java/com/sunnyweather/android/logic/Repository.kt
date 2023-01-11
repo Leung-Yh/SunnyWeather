028bdb09dd6b51820cf9f3bca328c39806f835bc
Leungyh <2111643216@qq.com>
2023-01-11 17:24:56 +0800

    加入显示天气信息的功能

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -1,9 +1,14 @@
 package com.sunnyweather.android.logic
 
 import androidx.lifecycle.liveData
+import com.sunnyweather.android.logic.dao.PlaceDao
 import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.logic.model.Weather
 import com.sunnyweather.android.logic.network.SunnyWeatherNetwork
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.CoroutineContext
 
 
 /**
@@ -30,19 +35,55 @@ object Repository {
      * 众所周知，Android是不允许在主线程中进行网络请求的，诸如读写数据库之类的本地数据操作也是
      * 不建议在主线程中进行的，因此非常有必要在仓库层进行一次线程转换
      */
-    fun searchPlace(query:String) = liveData(Dispatchers.IO) {
-        val result = try {
-            val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
-            if (placeResponse.status == "ok") {
-                val place = placeResponse.places
-                Result.success(place)
-            } else {
-                Result.failure(RuntimeException("response status is ${placeResponse.status}"))
-            }
-        } catch (e:Exception) {
-            Result.failure<List<Place>>(e)
+    fun searchPlace(query:String) = fire(Dispatchers.IO) {
+        val placeResponse = SunnyWeatherNetwork.searchPlaces(query)
+        if (placeResponse.status == "ok") {
+            val place = placeResponse.places
+            Result.success(place)
+        } else {
+            Result.failure(RuntimeException("response status is ${placeResponse.status}"))
         }
-        emit(result)
     }
+
+    fun refreshWeather(lng: String, lat: String) = fire(Dispatchers.IO) {
+        coroutineScope {
+            val deferredRealtime = async {
+                SunnyWeatherNetwork.getRealtimeWeather(lng, lat)
+            }
+            val deferredDaily = async {
+                SunnyWeatherNetwork.getDailyWeather(lng, lat)
+            }
+            val realtimeResponse = deferredRealtime.await()
+            val dailyResponse = deferredDaily.await()
+            if (realtimeResponse.status == "ok" && dailyResponse.status == "ok") {
+                val weather = Weather(realtimeResponse.result.realtime,
+                    dailyResponse.result.daily)
+                Result.success(weather)
+            } else {
+                Result.failure(
+                    RuntimeException(
+                        "realtime response status is ${realtimeResponse.status}" +
+                                "daily response status is ${dailyResponse.status}"
+                    )
+                )
+            }
+        }
+    }
+
+    private fun <T> fire(context: CoroutineContext, block: suspend () -> Result<T>) =
+        liveData<Result<T>>(context) {
+            val result = try {
+                block()
+            } catch (e: Exception) {
+                Result.failure<T>(e)
+            }
+            emit(result)
+        }
+
+    fun savePlace(place: Place) = PlaceDao.savePlace(place)
+
+    fun getSavedPlace() = PlaceDao.getSavedPlace()
+
+    fun isPlaceSaved() = PlaceDao.isPlaceSaved()
 
 }
--- app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/sunnyweather/android/logic/network/SunnyWeatherNetwork.kt
@@ -13,6 +13,14 @@ import kotlin.coroutines.suspendCoroutine
  */
 object SunnyWeatherNetwork {
 
+    private val weatherService = ServiceCreator.create(WeatherService::class.java)
+
+    suspend fun getDailyWeather(lng: String, lat: String) =
+        weatherService.getDailyWeather(lng, lat).await()
+
+    suspend fun getRealtimeWeather(lng: String, lat: String) =
+        weatherService.getRealtimeWeather(lng, lat).await()
+
     //PlaceService接口的动态代理对象
     private val placeService = ServiceCreator.create(PlaceService::class.java)
 
